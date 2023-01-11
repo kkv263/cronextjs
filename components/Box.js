a032340001e6639dafc44545127f043ca57d7f12
Kevin Vu <kevin.vu@crometrics.com>
2023-01-11 12:10:37 -0600

    addd back button

--- components/Box.js
+++ components/Box.js
@@ -1,7 +1,11 @@
 import styles from '../styles/About.module.css'
+import Link from 'next/link'
 
 export default function Box({ color }) {
   return (
-      <div>#{color}</div> 
+    <>
+      <div style={{ backgroundColor: '#'+color }} className={styles.box}>#{color}</div> 
+      <Link href=  "/">Back</Link>
+    </>
   )
 }
